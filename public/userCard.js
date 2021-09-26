c8834a7025821cdaaab2d79f0f0bd0f44ca031bc
fosemberg <fosemberg@gmail.com>
2021-09-26 17:24:45 +0300

    add userCard add toggleInfo with connectedCallback

--- public/userCard.js
+++ public/userCard.js
@@ -43,6 +43,9 @@ template.innerHTML = `
 class UserCard extends HTMLElement {
     constructor() {
         super();
+
+        this.showInfo = true;
+
         this.attachShadow({mode: 'open'})
         this.shadowRoot.appendChild(template.content.cloneNode(true))
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
@@ -50,11 +53,27 @@ class UserCard extends HTMLElement {
         console.log('constructor')
     }
 
+    toggleInfo = () => {
+        this.showInfo = !this.showInfo
+        const info = this.shadowRoot.querySelector('.info')
+        const toggleBtn = this.shadowRoot.querySelector('#toggle-info')
+
+        if (this.showInfo) {
+            info.style.display = 'block'
+            toggleBtn.innerText = 'Hide Info'
+        } else {
+            info.style.display = 'none'
+            toggleBtn.innerText = 'Show Info'
+        }
+    }
+
     connectedCallback() {
+        this.shadowRoot.querySelector('#toggle-info').addEventListener('click', this.toggleInfo)
         console.log('connectedCallback')
     }
 
     disconnectedCallback() {
+        this.shadowRoot.querySelector('#toggle-info').removeEventListener('click', this.toggleInfo)
         console.log('disconnectedCallback')
     }
 
